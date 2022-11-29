5fdf28f8e1f4d0691dca7337f2b9af47f0e8cf67
indyrajanuar <84507394+indyrajanuar@users.noreply.github.com>
2022-11-29 21:25:37 +0700

    Update knaps.py

--- knaps.py
+++ knaps.py
@@ -90,8 +90,8 @@ with preporcessing:
 with modeling:
     X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,random_state=4)
     sc = StandardScaler()
-    X_train = sc.fit_transform(x_train)
-    X_test = sc.transform(x_test)
+    X_train = sc.fit_transform(X_train)
+    X_test = sc.transform(X_test)
 
     st.write("""# Modeling """)
     st.subheader("Berikut ini adalah pilihan untuk Modeling")
